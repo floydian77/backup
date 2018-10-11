f82094fef7856df3586d847a87eb040d4b273876
Pieter <pieter@mlbrgn.nl>
2018-10-11 14:35:50 +0200

    Option: extra backups

--- backup.sh
+++ backup.sh
@@ -1,24 +1,48 @@
 #!/bin/bash
 # Backup script.
 
+# Read options
+while getopts r option
+do
+  case "${option}"
+    in
+    r) REGULAR=1;;
+  esac
+done
+
 # Read environment variables from file
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 export $(egrep -v '^#' $SCRIPTPATH/.env | xargs)
 
+function backup () {
+  /bin/mkdir -p $DESTINATION/$1/$STAMP
+  /bin/tar -czvf $DESTINATION/$1/$STAMP/content.tar.gz -C  $CONTENT .
+  /usr/bin/mysqldump --defaults-file=$SCRIPTPATH/.my.cnf $DATABASE > $DESTINATION/$1/$STAMP/dump.sql
+}
+
+function clean() {
+  if [ -d $DESTINATION/$1/$2* ]
+    then
+      /bin/rm -rf $DESTINATION/$1/$2*
+  fi
+}
+
 # Date stamp
 STAMP=`/bin/date +%Y-%m-%d_%H.%M.%S`
 
+# Extra backup (no daily/weekly/monthly)
+if [ -z $REGULAR ]
+then
+  backup "extra"
+  exit 0
+fi
+
 # Daily backup
-/bin/mkdir -p $DESTINATION/daily/$STAMP
-/bin/tar -czvf $DESTINATION/daily/$STAMP/content.tar.gz -C  $CONTENT .
-/usr/bin/mysqldump --defaults-file=$SCRIPTPATH/.my.cnf $DATABASE > $DESTINATION/daily/$STAMP/dump.sql
+backup "daily"
 
 # Daily cleanup
 LAST_WEEK=`/bin/date --date="1 week ago" +%Y-%m-%d`
-if [ -d $DESTINATION/daily/$LAST_WEEK* ]
-  then
-    /bin/rm -rf $DESTINATION/daily/$LAST_WEEK*
-fi
+clean "daily" "$LAST_WEEK"
 
 # Weekly backup
 if [ `/bin/date +%u` == $DAY_OF_WEEK ]
@@ -27,10 +51,7 @@ then
   /bin/ln $DESTINATION/daily/$STAMP/* $DESTINATION/weekly/$STAMP
 
   LAST_MONTH=`/bin/date --date="1 month ago" +%Y-%m-%d`
-  if [ -d $DESTINATION/weekly/$LAST_MONTH* ]
-    then
-      /bin/rm -rf $DESTINATION/weekly/$LAST_MONTH*
-  fi
+  clean "weekly" "$LAST_MONTH"
 fi
 
 # Monthly backup
